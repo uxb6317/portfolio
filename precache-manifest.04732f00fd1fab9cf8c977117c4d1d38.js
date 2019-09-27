419085ba5ca9e92c7cb9a316c5e37b5fcc03ef4a
Ulzii <uxb6317@rit.edu>
2019-09-26 20:01:22 -0400

    Updates

--- precache-manifest.04732f00fd1fab9cf8c977117c4d1d38.js
+++ precache-manifest.04732f00fd1fab9cf8c977117c4d1d38.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "192a1514200eb7b23eb665310bc35218",
+    "revision": "9342a97eed8b7a0849668a0b27929fa9",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "0338abd82d5ec971c2c6",
-    "url": "/portfolio/static/css/main.8744a6aa.chunk.css"
+    "revision": "929e073bcfbcce30faa1",
+    "url": "/portfolio/static/css/main.20cc8d02.chunk.css"
   },
   {
     "revision": "5a04e8f0d7e70343de33",
     "url": "/portfolio/static/js/2.17bf0ee8.chunk.js"
   },
   {
-    "revision": "0338abd82d5ec971c2c6",
-    "url": "/portfolio/static/js/main.8bed3c95.chunk.js"
+    "revision": "929e073bcfbcce30faa1",
+    "url": "/portfolio/static/js/main.c4d575e2.chunk.js"
   },
   {
     "revision": "927cfbd21ced720ad1d8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.096fda4bfac63a6abec20f013eb4ebf5.js"
+  "/portfolio/precache-manifest.04732f00fd1fab9cf8c977117c4d1d38.js"
 );
 
 self.addEventListener('message', (event) => {
